b8d9db0ca7361d8d420ca5a684391ebc503d8821
Matheus Silva <ms25022003@gmail.com>
2023-02-11 09:40:04 -0300

    feat: create page login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,48 @@
+import { Button } from 'components';
+import { InputText } from 'components/input/input-text/input-text.styles';
+import { FormEvent, useState } from 'react';
+import * as s from './login.style';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [messageError, setMessageError] = useState(
+    'Usuário não encontrado, tente novamente'
+  );
+  const [logged, setLogged] = useState(false);
+
+  const onSubmitForm = (e: FormEvent) => {
+    e.preventDefault();
+    setLogged(true);
+  };
+
+  return (
+    <s.ContainerPage>
+      <s.ContainerLogin onSubmit={onSubmitForm}>
+        <s.Title>
+          {!logged ? '¡Hola! Ingresá tu e-mail' : 'Digite sua senha'}
+        </s.Title>
+        {!logged ? (
+          <InputText
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="email"
+          />
+        ) : (
+          <InputText
+            value={password}
+            type="password"
+            onChange={e => setPassword(e.target.value)}
+            placeholder="senha"
+          />
+        )}
+        <Button variant="primary">Confirmar</Button>
+        {!logged && <Button variant="tertiary">Criar conta</Button>}
+        {messageError && <s.MessageError>{messageError}</s.MessageError>}
+      </s.ContainerLogin>
+    </s.ContainerPage>
+  );
+};
+
+export default Login;
