2bb24c346cf5823c60f7e830d8246141b968e1ea
jennimay <mayumi.jenni@gmail.com>
2023-02-23 20:45:27 -0300

    button test

--- src/components/button/__tests__/Button.spec.tsx
+++ src/components/button/__tests__/Button.spec.tsx
@@ -1,15 +1,59 @@
-import { screen } from '@testing-library/react';
 import { renderWithTheme } from '../../../utils/tests/helpers';
 import { Button } from '../Button';
+import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-describe('button', () => {
-  it('should render the medium button by default', () => {
-    renderWithTheme(<Button>Button</Button>);
-    expect(screen.getByRole('button', { name: /Button/i })).toBeInTheDocument();
+describe('<Button>', () => {
+  it('should render button by deafult theme', async () => {
+    const { getByText, getByRole } = renderWithTheme(<Button>button</Button>);
+
+    expect(getByRole('button')).toBeInTheDocument();
+    expect(getByText('button')).toBeInTheDocument();
+    expect(getByRole('button')).toHaveStyle({
+      backgroundColor: '#C1FD35',
+      padding: '1.25rem 0.875rem',
+      width: '22.5rem'
+    });
   });
+
   it('should render the small button', () => {
-    renderWithTheme(<Button size="xsmall">Button</Button>);
-    expect(screen.getByRole('button', { name: /Button/i })).toBeInTheDocument();
+    renderWithTheme(
+      <Button size="xsmall" variant="disabled">
+        Small Button
+      </Button>
+    );
+
+    expect(
+      screen.getByRole('button', { name: /Small Button/i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button')).toHaveStyle({
+      padding: '1.25rem 0.625rem'
+    });
+  });
+
+  it('should render the button with secondary (#201F22) background color', () => {
+    renderWithTheme(<Button variant="secondary">Button secondary</Button>);
+
+    const button = screen.getByRole('button');
+
+    expect(button).toHaveStyle({
+      backgroundColor: '#201F22'
+    });
+  });
+
+  it('should render the large button with tertiary(#CECECE) background color', () => {
+    renderWithTheme(
+      <Button variant="tertiary" size="large">
+        Button Large
+      </Button>
+    );
+
+    const btnLarge = screen.getByRole('button');
+
+    expect(btnLarge).toHaveStyle({
+      width: '31rem',
+      padding: '2.5rem 1.25rem',
+      backgroundColor: '#CECECE'
+    });
   });
 });
