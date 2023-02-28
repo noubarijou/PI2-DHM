62b5a9da890f366ca44206c3804cd8a45219a1df
jennimay <mayumi.jenni@gmail.com>
2023-02-28 02:00:16 -0300

    feat: nav link component

---
+++ src/components/sideMenu/NavLink/NavLink.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+import { NavLink as Link } from '../SideMenu.styles';
+
+type NavLinkProps = {
+  route: string;
+  children: React.ReactNode;
+};
+
+const NavLink = ({ route, children }: NavLinkProps) => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  return (
+    <Link href={route} className={currentRoute === route ? 'active' : ''}>
+      {children}
+    </Link>
+  );
+};
+
+export { NavLink };
