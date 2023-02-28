d22d1df9cc5aac92df421043932d026f20733b75
jennimay <mayumi.jenni@gmail.com>
2023-02-28 19:19:19 -0300

    refactor: head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,13 +36,20 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <title>Digital Money House</title>
+          <link
+            rel="shortcut icon"
+            href="/assets/images/LOGO-DMH-green.png"
+            type="image/png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
