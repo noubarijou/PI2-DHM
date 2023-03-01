39c9c835c9fa2e4f3888c0c9699aecfb1abcaeea
jennimay <mayumi.jenni@gmail.com>
2023-03-01 06:06:51 -0300

    refactor: changed s.ContainerPage to ContainerPage component

--- src/pages/wallet/wallet.styles.ts
+++ src/pages/wallet/wallet.styles.ts
@@ -3,28 +3,6 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import styled, { css } from 'styled-components';
 import theme from 'styles/theme';
 
-export const ContainerPage = styled.div`
-  ${({ theme }) => css`
-    min-height: ${theme.calc.body};
-    display: flex;
-    flex-direction: column;
-    padding: 1.25rem;
-    padding-bottom: 2.5rem;
-    align-items: center;
-    gap: 1.25rem;
-
-    @media (min-width: ${theme.screen.tablet}) {
-      padding-left: 16.875rem;
-      padding-top: 2.5rem;
-    }
-
-    @media (min-width: ${theme.screen.desktopXL}) {
-      padding-left: 22.25rem;
-      padding-top: 2.5rem;
-    }
-  `}
-`;
-
 export const PageTitle = styled.div`
   display: flex;
   align-items: center;
