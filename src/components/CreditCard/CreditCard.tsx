c9d5278c4bb798512e2ce33c785a2faf1f83840e
jennimay <mayumi.jenni@gmail.com>
2023-04-07 01:09:54 -0300

    style: show cvv in credit card

--- src/components/CreditCard/CreditCard.tsx
+++ src/components/CreditCard/CreditCard.tsx
@@ -1,31 +1,58 @@
+import { useEffect, useState } from 'react';
 import * as s from './CreditCard.style';
 import { FlagContainer } from './FlagContainer/FlagContainer';
 
 export type CreditCardProps = {
-  cardNumber?: number;
+  cardNumber?: string;
   cardName?: string;
   cardExpiration?: string;
   cvv?: number;
+  cvvOnFocus: boolean;
 };
 
 const CreditCard = ({
   cardNumber,
   cardName,
   cardExpiration,
-  cvv
+  cvv,
+  cvvOnFocus
 }: CreditCardProps) => {
+  const [cardNumMasked, setCardNumMasked] = useState('');
+  const cardNum = [
+    cardNumber?.slice(0, 4),
+    cardNumber?.slice(4, 8),
+    cardNumber?.slice(8, 12),
+    cardNumber?.slice(12)
+  ].join(' ');
+
+  useEffect(() => {
+    setCardNumMasked(cardNum);
+  });
+
   return (
     <>
-      <s.CreditCardContainer variant={!!cardNumber ? 'valid' : 'disabled'}>
-        <FlagContainer cardNumber={cardNumber} />
-        <s.NumberContainer>
-          {!!cardNumber ? cardNumber : '**** **** **** ****'}
-        </s.NumberContainer>
-        <s.InfoContainer>
-          <p>{!!cardName ? cardName : 'Nome do titular'}</p>
-          <span>{!!cardExpiration ? cardExpiration : 'MM/YY'}</span>
-        </s.InfoContainer>
-      </s.CreditCardContainer>
+      <div id="credit-card">
+        {!cvvOnFocus ? (
+          <s.CreditCardContainer variant={!!cardNumber ? 'valid' : 'disabled'}>
+            <FlagContainer cardNumber={Number(cardNumber)} />
+            <s.NumberContainer>
+              {!!cardNumber ? cardNumMasked : '**** **** **** ****'}
+            </s.NumberContainer>
+            <s.InfoContainer>
+              <p>{!!cardName ? cardName : 'Nome do titular'}</p>
+              <span>{!!cardExpiration ? cardExpiration : 'MM/YY'}</span>
+            </s.InfoContainer>
+          </s.CreditCardContainer>
+        ) : (
+          <s.CreditCardContainer
+            variant={!!cardNumber ? 'valid' : 'disabled'}
+            className="back"
+          >
+            <div></div>
+            <span>{!!cvv ? cvv : 'CVV'}</span>
+          </s.CreditCardContainer>
+        )}
+      </div>
     </>
   );
 };
